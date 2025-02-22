685998269a3c7433238ed16cf8d31e3bb0ea8c52
NahidaUrmi <nahidazamanurmi22@gmail.com>
2025-02-23 03:47:07 +0600

    Updated primenumber.cpp with new changes

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,10 +1,25 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-//write a function to print prime numbers from 1 to 100 and call it in the main function
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i <= num / 2; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
 
 int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
 
-    //call the function here
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is not a prime number." << endl;
+    }
+
     return 0;
 }
+
